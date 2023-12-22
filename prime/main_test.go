e8a5a71f18305aec06af028dc01690b1df3ab8d5
Qonaah Nurussalamah <qnurussalamah@gmail.com>
2023-12-22 14:40:14 +0700

    1 write test for prompt() intro() and checkNumbers()

--- prime/main_test.go
+++ prime/main_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"io"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -30,6 +34,90 @@ func Test_isPrime(t *testing.T) {
 
 		if e.msg != msg {
 			t.Errorf("%s: expected '%s' but got '%s'", e.name, e.msg, msg)
+		}
+	}
+}
+
+func Test_prompt(t *testing.T) {
+	// save a copy of os.Stdout
+	oldOut := os.Stdout
+
+	// create a read and write pipe
+	r, w, _ := os.Pipe()
+
+	// set os.Stdout to our write pipe
+	os.Stdout = w
+
+	prompt()
+
+	// close our writer
+	_ = w.Close()
+
+	// reset os.Stdout to what it was before
+	os.Stdout = oldOut
+
+	// read the output of our prompt() func from our read pipe
+	out, _ := io.ReadAll(r)
+
+	// perform our test
+	if string(out) != "-> " {
+		t.Errorf("incorrect prompt: expected -> but got %s", string(out))
+	}
+}
+
+func Test_intro(t *testing.T) {
+	// save a copy of os.Stdout
+	oldOut := os.Stdout
+
+	// create a read and write pipe
+	r, w, _ := os.Pipe()
+
+	// set os.Stdout to our write pipe
+	os.Stdout = w
+
+	intro()
+
+	// close our writer
+	_ = w.Close()
+
+	// reset os.Stdout to what it was before
+	os.Stdout = oldOut
+
+	// read the output of our prompt() func from our read pipe
+	out, _ := io.ReadAll(r)
+
+	// perform our test
+	if !strings.Contains(string(out), "Is it prime?") {
+		t.Errorf("intro text not correct; got %s", string(out))
+	}
+}
+
+func Test_checkNumbers(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{name: "empty", input: "", expected: "Please enter the whole number"},
+		{name: "zero", input: "0", expected: "0 is not prime number, by definition"},
+		{name: "one", input: "1", expected: "1 is not prime number, by definition"},
+		{name: "two", input: "2", expected: "2 is prime number"},
+		{name: "three", input: "3", expected: "3 is prime number"},
+		{name: "negative", input: "-1", expected: "negative number is not prime number, by definition"},
+		{name: "typed", input: "three", expected: "Please enter the whole number"},
+		{name: "decimal", input: "1.1", expected: "Please enter the whole number"},
+		{name: "quit", input: "q", expected: ""},
+		{name: "QUIT", input: "Q", expected: ""},
+		{name: "greek", input: "επτά", expected: "Please enter the whole number"},
+	}
+
+	for _, e := range tests {
+		input := strings.NewReader(e.input)
+		reader := bufio.NewScanner(input)
+		res, _ := checkNumbers(reader)
+
+		if !strings.EqualFold(res, e.expected) {
+			t.Errorf("%s: expected %s, but got %s", e.name, e.expected, res)
 		}
 	}
 }
