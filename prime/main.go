7340d417d16598ad0b3604040016dd071bafac9a
Qonaah Nurussalamah <qnurussalamah@gmail.com>
2023-12-22 14:15:06 +0700

    1 implement command prompt to interact wittth user

--- prime/main.go
+++ prime/main.go
@@ -1,6 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// print a welcome message
+	intro()
+
+	// create a channel to indicate when the user wants to quit
+	doneChan := make(chan bool)
+
+	// start a goroutine to read user input
+	go readUserInput(doneChan)
+
+	// block until the doneChan gets value
+	<-doneChan
+
+	// close the channel
+	close(doneChan)
+
+	// say goodbye
+	fmt.Println("Goodbye.")
+}
+
+func readUserInput(doneChan chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		res, done := checkNumbers(scanner)
+
+		if done {
+			doneChan <- true
+			return
+		}
+
+		fmt.Println(res)
+		prompt()
+	}
+}
+
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+	// read user input
+	scanner.Scan()
+
+	// check to see if user wants to quit
+	if strings.EqualFold(scanner.Text(), "q") {
+		return "", true
+	}
+
+	// try to convers what the user typed into an int
+	numToCheck, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		return "Please enter the whole number", false
+	}
+
+	_, msg := isPrime(numToCheck)
+
+	return msg, false
+}
 
 func isPrime(n int) (bool, string) {
 	if n == 0 || n == 1 {
@@ -18,4 +80,15 @@ func isPrime(n int) (bool, string) {
 	}
 
 	return true, fmt.Sprintf("%d is prime number", n)
+}
+
+func intro() {
+	fmt.Println("Is it prime?")
+	fmt.Println("------------")
+	fmt.Println("Enter a number, and we will tell you if it is prime number")
+	prompt()
+}
+
+func prompt() {
+	fmt.Print("-> ")
 }
