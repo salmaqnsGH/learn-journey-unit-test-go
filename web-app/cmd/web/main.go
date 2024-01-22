92902b6067125a8200c0b54eba9f7a9602ab4dc6
tpid-salma <salma@technopartner.co.id>
2024-01-22 16:26:34 +0700

    4 setup database connection

--- web-app/cmd/web/main.go
+++ web-app/cmd/web/main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/alexedwards/scs/v2"
+	"github.com/joho/godotenv"
 )
 
 type application struct {
+	DSN     string
+	DB      *sql.DB
 	Session *scs.SessionManager
 }
 
 func main() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	// set up an app config
 	app := application{}
+
+	dsn := os.Getenv("POSTGRES_DSN")
+	flag.StringVar(&app.DSN, "dsn", dsn, "Postgres connection")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = conn
 
 	// get a session manager
 	app.Session = getSession()
@@ -22,7 +45,7 @@ func main() {
 	log.Println("Starting server on port 8080...")
 
 	// start the server
-	err := http.ListenAndServe(":8080", app.routes())
+	err = http.ListenAndServe(":8080", app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
