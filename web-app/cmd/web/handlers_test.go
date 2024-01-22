aac85d2cffc81b6a0c91a0346cdf40460c8254a7
tpid-salma <salma@technopartner.co.id>
2024-01-22 14:41:09 +0700

    3 testing the render functtion with a bad template

--- web-app/cmd/web/handlers_test.go
+++ web-app/cmd/web/handlers_test.go
@@ -25,8 +25,6 @@ func Test_application_handlers(t *testing.T) {
 	ts := httptest.NewTLSServer(routes)
 	defer ts.Close()
 
-	pathToTemplates = "./../../templates/"
-
 	// range through test data
 	for _, e := range theTests {
 		resp, err := ts.Client().Get(ts.URL + e.url)
@@ -78,6 +76,20 @@ func TestAppHome(t *testing.T) {
 		if !strings.Contains(string(body), e.expectedHTML) {
 			t.Errorf("%s: did not find %s in response body", e.name, e.expectedHTML)
 		}
+	}
+}
+
+func TestApp_renderWithBadTemplate(t *testing.T) {
+	// set template path to a location with a bad template
+	pathToTemplates = "./testdata/"
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	req = addContextAndSessionToRequest(req, app)
+	rr := httptest.NewRecorder()
+
+	err := app.render(rr, req, "bad.page.gohtml", &TemplateData{})
+	if err == nil {
+		t.Error("expected error from bad template, but did not get one")
 	}
 }
 
