d6622c918a55b043ab4b7adcb36df00f292435c3
tpid-salma <salma@technopartner.co.id>
2024-01-22 14:14:39 +0700

    3 update test for homepage

--- web-app/cmd/web/handlers_test.go
+++ web-app/cmd/web/handlers_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -15,7 +18,6 @@ func Test_application_handlers(t *testing.T) {
 		{"home", "/", http.StatusOK},
 		{"404", "/fish", http.StatusNotFound},
 	}
-
 
 	routes := app.routes()
 
@@ -37,4 +39,41 @@ func Test_application_handlers(t *testing.T) {
 			t.Errorf("for %s: expected status %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 		}
 	}
+}
+
+func TestAppHome(t *testing.T) {
+	// create request
+	req, _ := http.NewRequest("GET", "/", nil)
+
+	// add session to context
+	req = addContextAndSessionToRequest(req, app)
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(app.Home)
+
+	handler.ServeHTTP(rr, req)
+
+	// check status code
+	if rr.Code != http.StatusOK {
+		t.Errorf("TestAppHome returned wrong status code; expected 200 but got %d", rr.Code)
+	}
+
+	body, _ := io.ReadAll(rr.Body)
+	if !strings.Contains(string(body), `<small>From session: `) {
+		t.Error("did not find correct text in html")
+	}
+}
+
+func getCtx(req *http.Request) context.Context {
+	ctx := context.WithValue(req.Context(), contextUserKey, "unknown")
+	return ctx
+}
+
+func addContextAndSessionToRequest(req *http.Request, app application) *http.Request {
+	req = req.WithContext(getCtx(req))
+
+	ctx, _ := app.Session.Load(req.Context(), req.Header.Get("X-Session"))
+
+	return req.WithContext(ctx)
 }
