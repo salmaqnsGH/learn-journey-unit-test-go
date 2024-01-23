b05167451964b184961ac25a3617ba56e10a54f4
tpid-salma <salma@technopartner.co.id>
2024-01-23 16:53:08 +0700

    4 testing the Login handler

--- web-app/cmd/web/setup_test.go
+++ web-app/cmd/web/setup_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"log"
 	"os"
 	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/salmaqnsGH/unit-test/pkg/db"
 )
 
 var app application
@@ -10,7 +14,25 @@ var app application
 func TestMain(m *testing.M) {
 	pathToTemplates = "./../../templates/"
 
+	// Load .env file
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := os.Getenv("POSTGRES_DSN")
+
 	app.Session = getSession()
+	app.DSN = dsn
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	app.DB = db.PostgresConn{DB: conn}
 
 	os.Exit(m.Run())
 }
