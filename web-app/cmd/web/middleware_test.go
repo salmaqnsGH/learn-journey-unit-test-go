2cfda480646a03c14e807f0f218c041fcb41c5ec
tpid-salma <salma@technopartner.co.id>
2024-01-24 08:48:34 +0700

    4 testing middleware

--- web-app/cmd/web/middleware_test.go
+++ web-app/cmd/web/middleware_test.go
@@ -6,6 +6,8 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"github.com/salmaqnsGH/unit-test/pkg/data"
 )
 
 func Test_application_addIPToContext(t *testing.T) {
@@ -72,5 +74,39 @@ func Test_application_ipFromContext(t *testing.T) {
 	// perform the test
 	if !strings.EqualFold("whatever", ip) {
 		t.Error("wrong value returned from context")
+	}
+}
+
+func Test_app_auth(t *testing.T) {
+	nexhHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	var tests = []struct {
+		name   string
+		isAuth bool
+	}{
+		{"logged in", true},
+		{"not logged in", false},
+	}
+
+	for _, e := range tests {
+		handlerToTest := app.auth(nexhHandler)
+		req := httptest.NewRequest("GET", "http://testing", nil)
+		req = addContextAndSessionToRequest(req, app)
+		if e.isAuth {
+			app.Session.Put(req.Context(), "user", data.User{ID: 1})
+		}
+
+		rr := httptest.NewRecorder()
+		handlerToTest.ServeHTTP(rr, req)
+
+		if e.isAuth && rr.Code != http.StatusOK {
+			t.Errorf("%s: expected status code of 200 but got %d", e.name, rr.Code)
+		}
+
+		if !e.isAuth && rr.Code != http.StatusTemporaryRedirect {
+			t.Errorf("%s: expected status code 307, but got %d", e.name, rr.Code)
+		}
 	}
 }
